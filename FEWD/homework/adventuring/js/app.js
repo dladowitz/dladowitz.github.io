5cd1c4b5a2c1858d56dde6dd2ad65c3c85b2e56e
David Ladowitz <david@ladowitz.com>
2013-08-02 23:02:30 -0700

    added account info and axajed search box

--- FEWD/homework/adventuring/js/app.js
+++ FEWD/homework/adventuring/js/app.js
@@ -18,5 +18,44 @@ $(document).ready(function() {
     function(){$(this).find('.course_overlay').slideDown()},
     function(){$(this).find('.course_overlay').slideUp()}
   );
+
+  $('.show_company_info').click(function(){
+    $('.show_company').css('display', 'block');
+    $('.edit_company').css('display', 'none');
+    $('.show_classes').css('display', 'none');
+    $('.user_permissions').css('display', 'none');
+  });
+
+  $('.edit_company_info').click(function(){
+    $('.edit_company').css('display', 'block');
+    $('.show_company').css('display', 'none');
+    $('.show_classes').css('display', 'none');
+    $('.user_permissions').css('display', 'none');
+  });
+
+  $('.show_class_info').click(function(){
+    $('.show_company').css('display', 'none');
+    $('.edit_company').css('display', 'none');
+    $('.show_classes').css('display', 'block');
+    $('.user_permissions').css('display', 'none');
+  });
+
+  $('.show_user_permissions').click(function(){
+    $('.show_company').css('display', 'none');
+    $('.edit_company').css('display', 'none');
+    $('.show_classes').css('display', 'none');
+    $('.user_permissions').css('display', 'block');
+  });
+
+  $(function() {
+    var availableSports = ["Rock Climbing", "Sailing", "Scuba Diving", "Canyoneering", "Mountianeering", "Skydiving", "Paragliging", "Mountain Biking", "White Water Rafting", "Hang Gliding",
+      "Base Jumping", "Kayaking", "Motorcyle Racing", "Auto Racing", "Cave Diving", "Bull Riding", "Motocross"];
+
+    $( ".search_box" ).autocomplete({
+      source: availableSports
+    });
+  });
+
+
 });
 
